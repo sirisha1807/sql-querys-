79dc8337cb151c041a61425dfeb0aeb1b159f278
sirisha1807 <90616237+sirisha1807@users.noreply.github.com>
2021-09-22 11:30:35 +0530

    sql assignments

---
+++ sql day1.sql
@@ -0,0 +1,35 @@
+create database stud;
+use stud;
+
+select * from stud;
+
+select * from stud where markes> 500 and markes<=900;
+
+select markes from stud where markes>600 and markes<=900;
+
+select * from stud where name='killer';
+
+select * from stud where name like '%ri%';
+
+
+
+select * from stud where markes>600 and name like '%ri%' or '%ll%';
+
+select * from stud where id>3 order by id desc;
+select count(*) from stud where id>3 order by id desc;
+select * from stud group by name;
+
+select distinct name from stud;
+drop table dept;
+
+update stud set deptid=6 where id=4;
+
+alter table stud add deptid int;
+
+delete from dept where deptid=2;
+
+
+
+
+
+
---
+++ sql day2.sql
@@ -0,0 +1,19 @@
+JIONS 
+create table sales(salesid int not null primary key, salesname varchar(20), address varchar(20));
+
+insert into sales values (1,'dd', 'bang'),(2,'gg','chin'),(3,'kkk', 'devana'),(4,'ll','viji');
+
+select * from sales s;
+
+create table ordersales (id int primary key , productname varchar(20), address varchar(20), salesid int,
+ 
+constraint FK_order foreign key(salesid) REFERENCES sales(salesid));
+
+insert into ordersales values (1,'glass', 'bang', 3),(2,'light','chin',2),(3,'kill', 'devana', 4),(4,'lol','viji',1);
+
+select * from ordersales p;
+select s.salesid,s.salesname from sales s inner join ordersales p on p.salesid=s.salesid;
+select s.salesid,s.salesname from sales s outer join ordersales p on p.salesid=s.salesid;
+select s.salesid,s.salesname from sales s right join ordersales p on p.salesid=s.salesid;
+select s.salesid,s.salesname from sales s full outer join ordersales p on p.salesid=s.salesid;
+
